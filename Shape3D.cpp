717022a8c86ad6a5b226733d4cebc3b733acf225
MoXuToS <moxutosic@gmail.com>
2024-01-28 20:48:56 +0300

    main

--- Shape3D.cpp
+++ Shape3D.cpp
@@ -9,28 +9,45 @@ vector<double> Circle::getPoint(double t) const
 {
     double x = radius * cos(t);
     double y = radius * sin(t);
-    double z = 0;
+    double z = 0.0;
     return {x,y,z};
 };
 vector<double> Circle::getDerivative(double t) const 
 {
-        
+    double dx = -radius * sin(t);
+    double dy = radius * cos(t);
+    double dz = 0.0;
+    return {dx,dy,dz};
 };
 
 vector<double> Ellipse::getPoint(double t) const
 {
-
+    double x = radiusX * sin(t);
+    double y = radiusY * cos(t);
+    double z = 0.0;
+    return {x,y,z};
 };
+
 vector<double> Ellipse::getDerivative(double t) const
 {
-
+    double dx = -radiusX * cos(t);
+    double dy = radiusY * sin(t);
+    double dz = 0.0;
+    return {dx,dy,dz};
 }; 
 
 vector<double> Helix::getPoint(double t) const 
 {
-
+    double x = radius * cos(t);
+    double y = radius * sin(t);
+    double z = step * t / (2*M_PI);
+    return {x,y,z};
 };
+
 vector<double> Helix::getDerivative(double t) const
 {
-
+    double dx = -radius * sin(t);
+    double dy = radius * cos(t);
+    double dz = step / (2*M_PI);
+    return {dx,dy,dz};
 };
