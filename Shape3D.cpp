241cc6b9887d7ac1f515e88e3fdf062b8c2ad866
MoXuToS <moxutosic@gmail.com>
2024-01-28 20:35:56 +0300

    Main

--- Shape3D.cpp
+++ Shape3D.cpp
@@ -1,3 +1,36 @@
 #include <Shape3D.h>
 #include <iostream>
 #include <vector>
+#include <cmath>
+
+using namespace std;
+
+vector<double> Circle::getPoint(double t) const
+{
+    double x = radius * cos(t);
+    double y = radius * sin(t);
+    double z = 0;
+    return {x,y,z};
+};
+vector<double> Circle::getDerivative(double t) const 
+{
+        
+};
+
+vector<double> Ellipse::getPoint(double t) const
+{
+
+};
+vector<double> Ellipse::getDerivative(double t) const
+{
+
+}; 
+
+vector<double> Helix::getPoint(double t) const 
+{
+
+};
+vector<double> Helix::getDerivative(double t) const
+{
+
+};
--- Shape3D.h
+++ Shape3D.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 
@@ -7,29 +8,38 @@ class Curve3D
 {
 public:
     virtual ~Curve3D() {}
-    virtual vector<double> getPoint(double t);
+    virtual vector<double> getPoint(double t) const = 0;
+    virtual vector<double> getDerivative(double t) const = 0;
 };
 
 class Circle : Curve3D
 {
 public:
-
+    Circle(double radius) : radius(radius) {}
+    vector<double> getPoint(double t) const override;
+    vector<double> getDerivative(double t) const override;
 private:
-
+    double radius;
 };
 
 class Ellipse : Curve3D
 {
 public:
-
+    Ellipse(double radiusX,double radiusY) : radiusX(radiusX), radiusY(radiusY) {}
+    vector<double> getPoint(double t) const override;
+    vector<double> getDerivative(double t) const override;
 private:
-
+    double radiusX;
+    double radiusY;
 };
 
 class Helix : Curve3D
 {
 public:
-
+    Helix(double radius,double step) : radius(radius), step(step) {}
+    vector<double> getPoint(double t) const override;
+    vector<double> getDerivative(double t) const override;
 private:
-
+    double radius;
+    double step;
 };
